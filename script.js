1ac6b89ad237f168e9314f8e186855cb145d5373
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-01 14:24:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,11 +33,27 @@ class Cat extends Animal {
 		console.log("purr");
 	}
 }
+// Creating an instance of Cat
+const myCat = new Cat("Siamese");
+myCat.makeSound();
+// Expected Output: The Siamese makes a sound
+
+myCat.purr();
+// Expected Output: purr
+
+// Creating an instance of Dog
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound();
+// Expected Output: The Golden Retriever makes a sound
+
+myDog.bark();
+// Expected Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
 
 
 
