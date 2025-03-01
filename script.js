2dbdd00b643c73ebf1d12f8a17c2b0c05359b2dd
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-01 13:24:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
-//complete this code
+// Parent class: Animal
 class Animal {
-	constructor(species){
-		this._species = species;
-	}
+  private _species: string; // Private property to store species
 
-	get.species(){
-	  return this._species;
-	}
+  constructor(species: string) {
+    this._species = species;
+  }
 
-	makeSound(){
-		return `The ${this._species} makes a sound `
-	}
+  // Getter for species
+  get species(): string {
+    return this._species;
+  }
+
+  // Method to make a sound
+  makeSound(): void {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
-class Dog extends Animal {
-	constructor(species = "Golden Retriever"){
-		super(species);
-	}
-	
-
-	bark(){
-		return `woof`;
-	}
-}
-
+// Child class: Cat
 class Cat extends Animal {
-	constructor(species = "Siamese"){
-		super(species);
-	}
+  constructor(species: string) {
+    super(species);
+  }
 
-	purr(){
-		return `purr`;
-	}
+  // Method specific to Cat
+  purr(): void {
+    console.log("purr");
+  }
+}
+
+// Child class: Dog
+class Dog extends Animal {
+  constructor(species: string) {
+    super(species);
+  }
+
+  // Method specific to Dog
+  bark(): void {
+    console.log("woof");
+  }
 }
 
 // Do not change the code below this line
